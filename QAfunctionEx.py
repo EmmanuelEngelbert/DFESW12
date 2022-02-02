fc5fafb25e021acdeb868b52da6a4c376819a331
EmmanuelEngelbert <emmanuel.engelbert@outlook.com>
2022-02-02 16:34:09 +0000

    corrected errors with the code

--- QAfunctionEx.py
+++ QAfunctionEx.py
@@ -1,6 +1,7 @@
 def ictGrade(hScore,aScore,eScore):  
     if hScore > 25 or aScore > 50 or eScore > 100: 
-        print("Error: invalid score/s provided") 
+        print("Error: invalid score/s provided")  
+        return 0
     else:
         return round((100/3)*(hScore/25 + aScore/50 + eScore/100))
 
@@ -25,6 +26,6 @@ var1 = ictGrade(homeworkScore,assesmentScore,examScore)
 var2 = gradeFunction(var1)
 
 if homeworkScore > 25 or assesmentScore > 50 or examScore >100:
-    print(studentName, "your %", "score could not be determined") 
+    print(studentName, "your","%", "score could not be determined") 
 else:
-    print(studentName, "your score was", var1,"%", ", Grade =", var2)
+    print(studentName, "your score was", var1,"%", "Grade =", var2)
